8da5354d84efe6b9f85524cc8a5a5896a49a6063
EdwardYUCU <151100932+EdwardYUCU@users.noreply.github.com>
2023-11-18 05:21:29 +0000

    new file:   .vscode/tasks.json modified:   q5 modified:   q5.c

--- q5.c
+++ q5.c
@@ -3,11 +3,12 @@
 #include <string.h>
 #define MAXSIZE 100
 
-char* addition(char* a, char* b);
+char *addition(char *a, char *b);
 
-int main(void) {
-    char a[MAXSIZE] = { 0 }, b[MAXSIZE] = { 0 }, n[MAXSIZE] = { 0 };
-    char* sum;
+int main(void)
+{
+    char a[MAXSIZE] = {0}, b[MAXSIZE] = {0}, n[MAXSIZE] = {0};
+    char *sum;
 
     scanf("%s", a);
     scanf("%s", b);
@@ -18,32 +19,39 @@ int main(void) {
     return 0;
 }
 
-char* addition(char a[], char b[]) {
-    char* result;
+char *addition(char *a, char *b)
+{
+    char *result;
     char rest = 0;
     int i, j;
-    result = (char*)malloc(sizeof(char) * MAXSIZE);
+    result = (char *)malloc(sizeof(char) * MAXSIZE);
     memset(result, 0, MAXSIZE);
     for (i = 0; a[i]; i++)
         ;
     for (j = 0; b[j]; j++)
         ;
 
-    if (i > j) {
-        for (i--, j--; j >= 0; i--, j--) {
+    if (i > j)
+    {
+        for (i--, j--; j >= 0; i--, j--)
+        {
             result[i] = (a[i] + b[j] + rest) % 10;
             rest = (a[i] + b[j]) / (char)10;
         }
-        for (; i >= 0; i--) {
+        for (; i >= 0; i--)
+        {
             result[i] = a[i];
         }
     }
-    else {
-        for (i--, j--; i >= 0; i--, j--) {
+    else
+    {
+        for (i--, j--; i >= 0; i--, j--)
+        {
             result[j] = (a[i] + b[j] + rest) % 10;
             rest = (char)((a[i] + b[j]) / 10);
         }
-        for (; j >= 0; j--) {
+        for (; j >= 0; j--)
+        {
             result[j] = b[j];
         }
     }

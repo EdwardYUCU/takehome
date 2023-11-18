4ec14a6f4f332c6fe039985dc62ce66da2e7b5af
EdwardYUCU <151100932+EdwardYUCU@users.noreply.github.com>
2023-11-18 06:43:45 +0000

    modified, with addition correct:   q5 modified:   q5.c modified:   q5.o

--- q5.c
+++ q5.c
@@ -1,59 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define DEBUG(X, Y) printf("%s (%d): %s = %d. %s = %d\n", \
+                 __FUNCTION__, __LINE__, #X, X, #Y, Y)
 #define MAXSIZE 100
+#define _DEBUG_
 
-char *addition(char *a, char *b);
 
-int main(void)
-{
-    char a[MAXSIZE] = {0}, b[MAXSIZE] = {0}, n[MAXSIZE] = {0};
-    char *sum;
+char* addition(char* a, char* b);
+char* division(char* sum, char* n);
+
+int main(void) {
+    char a[MAXSIZE] = { 0 }, b[MAXSIZE] = { 0 }, n[MAXSIZE] = { 0 };
+    char* sum;
+    char* reminder;
 
     scanf("%s", a);
     scanf("%s", b);
     scanf("%s", n);
+
     sum = addition(a, b);
-    printf("%s", sum);
+    reminder = division(sum, n);
 
     return 0;
 }
 
-char *addition(char *a, char *b)
-{
-    char *result;
+char* addition(char* a, char* b) {
+    char* result;
     char rest = 0;
     int i, j;
-    result = (char *)malloc(sizeof(char) * MAXSIZE);
+    result = (char*)malloc(sizeof(char) * MAXSIZE);
+    if (result == NULL) {
+        return NULL;
+    }
     memset(result, 0, MAXSIZE);
+
     for (i = 0; a[i]; i++)
         ;
     for (j = 0; b[j]; j++)
         ;
 
-    if (i > j)
-    {
-        for (i--, j--; j >= 0; i--, j--)
-        {
-            result[i] = (a[i] + b[j] + rest) % 10;
-            rest = (a[i] + b[j]) / (char)10;
+    if (i > j) {
+        for (i--, j--; j >= 0; i--, j--) {
+            result[i] = (a[i] - '0' + b[j] - '0' + rest) % 10 + '0';
+#ifdef _DEBUG_
+            DEBUG(result[i], i);
+#endif
+            rest = (a[i] - '0' + b[j] - '0') / 10;
         }
-        for (; i >= 0; i--)
-        {
-            result[i] = a[i];
+        for (; i >= 0; i--) {
+            result[i] = a[i] + '0';
+#ifdef _DEBUG_
+            DEBUG(result[i], i);
+#endif
         }
     }
-    else
-    {
-        for (i--, j--; i >= 0; i--, j--)
-        {
-            result[j] = (a[i] + b[j] + rest) % 10;
-            rest = (char)((a[i] + b[j]) / 10);
+    else {
+        for (i--, j--; i >= 0; i--, j--) {
+            result[j] = (a[i] - '0' + b[j] - '0' + rest) % 10 + '0';
+#ifdef _DEBUG_
+            DEBUG(result[j], j);
+#endif
+            rest = (a[i] - '0' + b[j] - '0') / 10;
         }
-        for (; j >= 0; j--)
-        {
-            result[j] = b[j];
+        for (; j >= 0; j--) {
+            result[j] = b[j] + '0';
+#ifdef _DEBUG_
+            DEBUG(result[j], j);
+#endif
         }
     }
     return result;
+}
+
+char* division(char* sum, char* n) {
+    char* result;
+    result = (char*)malloc(sizeof(char) * MAXSIZE);
+    if (result == NULL) {
+        return NULL;
+    }
+    memset(result, 0, MAXSIZE);
+
+
+
+    return result;
+
 }
